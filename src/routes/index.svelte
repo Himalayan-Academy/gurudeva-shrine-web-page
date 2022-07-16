5e9ae6830b960e43ede8d1721d99b8103d023fad
BrahmachariAkash <102331184+akash10008@users.noreply.github.com>
2022-07-16 10:04:14 -1000

    iamges now switch

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,20 @@
 <script>
-    let current_gurudeva_portrait = "/gurudeva.jpg"
+    import dayjs from 'dayjs'
+
+    const number_of_gurudeva_portraits = 3
+
+
+    let image_number = 1
+    $: current_gurudeva_portrait = `/gurudeva${image_number}.jpg`
+
+    setInterval(() => {
+        if (image_number == number_of_gurudeva_portraits){
+            image_number = 1
+        } else {
+            image_number+=1;
+        }
+    }, 2000)
+
 </script>
 
 <div style="background-image: url({current_gurudeva_portrait})" class="background" >
